e8af34e3162a8a7da491282fd6e304bef758af51
Negin Valizadegan <valizad2@biologin-hpcbio.igb.illinois.edu>
2021-09-16 21:46:33 -0500

    #36 Added CHM13 blast script

--- annotation.sh
+++ annotation.sh
@@ -15,9 +15,9 @@
 # HPCBio UIUC Annotation pipeline; Created by Negin Valizadegan Sep 2, 2021; valizad2@illinois.edu
 
 ##############################################################################
-##																			##
-##			           GENERAL WRAPPER RELATED SCRIPTS	                    ##
-##																			##	
+##																		                                    	##
+##			           GENERAL WRAPPER RELATED SCRIPTS	                        ##
+##																			                                    ##	
 ##############################################################################
 
 # Set fancy fonts for the help message ------
@@ -27,18 +27,20 @@ REV=`tput smso`
 
 # Help ------
 function HELP {
-  echo "${BOLD}Help documentation for the HPCBio UIUC Annotation pipeline${NORM}"
   echo ""
-  echo "The following options must be specified:"
-  echo "${REV}-d${NORM}   The full path to the main results directory${NORM}"  
+  echo "${BOLD}Help Documentation for the HPCBio UIUC Annotation Pipeline${NORM}"
+  echo ""
+  echo "The Following Options Must Be Specified:"
+  echo "${REV}-d${NORM}   The full path to the main results directory${NORM} (Required)"  
   #echo "${REV}-s${NORM}   TRUE for single-end and FALSE for paired-end${NORM}"
   #echo "${REV}-f${NORM}   Adapter/primer sequence to be trimmed from R1${NORM}"
   #echo "${REV}-r${NORM}   Adapter/primer sequence to be trimmed from R2${NORM}"
   #echo "${REV}-l${NORM}   DADA2 truncation length for R1${NORM}"
   #echo "${REV}-n${NORM}   DADA2 truncation length for R2${NORM}"
-  echo "${REV}-h${NORM}   Displays this help message without complaining about lack of arguments"
+  echo "${REV}-h${NORM}   Displays this help message without complaints (Optional)"
   echo ""
-  echo "Example: sbatch annotation.sh -d /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/results/"
+  echo "[ ${NORM}${BOLD}Example:${NORM} sbatch annotation.sh -d /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/results/ ]"
+  echo ""
   exit 1
 }
 
@@ -46,7 +48,9 @@ function HELP {
 # Check the number of arguments. If none are passed, print message and exit ------
 NUMARGS=$#
 if [ $NUMARGS -eq 0 ]; then
-  echo "Did not pass any arguments, you probably need help"
+  echo ""
+  echo "You Did Not Pass Any Arguments. Please Specify the Arguments Below:" 
+  echo ""
   HELP
 fi
 
@@ -107,9 +111,9 @@ fi
 
 
 ##############################################################################
-##																			##
-##			        STEP 1: LOAD THE NECESSARY MODULES                      ##
-##																			##	
+##																			                                    ##
+##			           STEP 1: LOAD THE NECESSARY MODULES                       ##
+##																			                                    ##	
 ##############################################################################
 setup ()
 {
@@ -123,53 +127,17 @@ module purge
 module load seqkit/0.12.1
 module load BLAST+/2.10.1-IGB-gcc-8.2.0
 
-echo "Necessary modules are loaded"
+echo "- Necessary modules are loaded"
 
 }
 
 ##############################################################################
-##																			##
-##				    STEP 2: FILTER THE ASSEMBLY FILES                       ##
-##																			##	
+##																			                                    ##
+##				            STEP 2: FILTER THE ASSEMBLY FILES                     ##
+##																			                                    ##	
 ##############################################################################
 
 filter ()
-{
-
-# ----- Set working directory -----
-cd ${OPT_d}/assembly/Final-Assembly/
-
-echo "Input directory is set to" | tr '\n' ' ' && pwd
-
-# Create a directory for outputs ----
-mkdir -p ../../annotation/seqkit/masurca
-mkdir -p ../../annotation/seqkit/megahit
-
-# ----- Filter sequences < 500bp length -----
-echo "Start of filtering process masurca"
-
-# Masurca -----
-cd masurca
-echo "Now directory is set to" | tr '\n' ' ' && pwd
-
-seqkit seq --min-len 500 --remove-gaps HG03563.masurca.final.fasta > ../../../annotation/seqkit/masurca/HG03563.filtered.fasta
-echo "[Now creating stats on filtered fasta files]"
-seqkit stats HG03563.masurca.final.fasta >> ../../../annotation/seqkit/masurca/HG03563.stats_before_filter.masurca.txt
-seqkit stats ../../../annotation/seqkit/masurca/HG03563.filtered.fasta >> ../../../annotation/seqkit/masurca/HG03563.stats_after_filter.masurca.txt
-
-
-echo "End of filtering process"
-
-}
-
-
-##############################################################################
-##																			##
-##	             STEP 3: CREATE BLAST DATABASE (GRCh38)                     ##
-##																			##	
-##############################################################################
-
-blastdbGRCh38 ()
 {
 
 # ----- Set working directory -----
@@ -177,30 +145,82 @@ cd ${OPT_d}
 
 echo "Input directory is set to" | tr '\n' ' ' && pwd
 
-# ----- Creat Blast database from GRC38.decoy.hla -----
- makeblastdb -in ../GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa -parse_seqids -title "GRCh38.decoy.hla" -dbtype nucl
+# Create a directory for outputs ----
+mkdir -p annotation/seqkit/masurca
+mkdir -p annotation/seqkit/megahit
 
-#Building a new DB, current time: 09/03/2021 18:47:07
-#New DB name:   /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa
-#New DB title:  GRCh38.decoy.hla
-#Sequence type: Nucleotide
-#Keep MBits: T
-#Maximum file size: 1000000000B
-#Adding sequences from FASTA; added 3366 sequences in 30.3125 seconds.
+# ----- Filter sequences < 500bp length -----
+echo "Start of filtering process masurca"
 
-# ----- Creat Blast database from GRC38.decoy.hla -----
-makeblastdb -in ../GRCh38.p0/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -parse_seqids -title "GRCh38.p0.no.decoy.hla" -dbtype nucl
+# Masurca -----
+cd masurca
+
+echo "[Removing reads with length < 500]"
+
+seqkit seq --min-len 500 --remove-gaps assembly/Final-Assembly/HG03563.masurca.final.fasta > annotation/seqkit/masurca/HG03563.filtered.fasta
+
+echo "[Creating stats on fasta files]"
+
+seqkit stats assembly/Final-Assembly/HG03563.masurca.final.fasta >> annotation/seqkit/masurca/HG03563.stats_before_filter.masurca.txt
+seqkit stats annotation/seqkit/masurca/HG03563.filtered.fasta >> annotation/seqkit/masurca/HG03563.stats_after_filter.masurca.txt
+
+echo "End of Filtering Process"
 
 }
 
 
 ##############################################################################
 ##																			                                    ##
-##			   STEP 4: RUN BLAST ON FILTERED READS (GRCh38)                     ##
+##	               STEP 3: CREATE BLAST DATABASE (GRCh38)                   ##
 ##																			                                    ##	
 ##############################################################################
 
-blastGRCh38 ()
+blastdb.GRCh38 ()
+{
+
+# ----- Set working directory -----
+cd ${OPT_d}
+
+echo "Input directory is set to" | tr '\n' ' ' && pwd
+
+
+# ----- Creat Blast database from GRCh38.decoy.hla [?] -----
+
+makeblastdb -in ../GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa -parse_seqids -title "GRCh38.decoy.hla" -dbtype nucl
+
+# Building a new DB, current time: 09/13/2021 18:24:45
+# New DB name:   /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa
+# New DB title:  GRCh38.decoy.hla
+# Sequence type: Nucleotide
+# Deleted existing Nucleotide BLAST database named /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/GRCh38/GRCh38_full_analysis_set_plus_decoy_hla.fa
+# Keep MBits: T
+# Maximum file size: 1000000000B
+# Adding sequences from FASTA; added 3366 sequences in 29.3228 seconds.
+
+
+# ----- Creat Blast database from GRCh38.no.decoy.hla [GRCh38.p0; GCA_000001405.15] -----
+
+# wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+makeblastdb -in ../GRCh38.p0/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -parse_seqids -title "GRCh38.p0.no.decoy.hla" -dbtype nucl
+
+# Building a new DB, current time: 09/13/2021 18:25:15
+# New DB name:   /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/GRCh38.p0/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+# New DB title:  GRCh38.p0.no.decoy.hla
+# Sequence type: Nucleotide
+# Keep MBits: T
+# Maximum file size: 1000000000B
+# Adding sequences from FASTA; added 195 sequences in 28.1475 seconds.
+
+}
+
+
+##############################################################################
+##																			                                    ##
+##			        STEP 4: RUN BLAST ON FILTERED READS (GRCh38)                ##
+##																			                                    ##	
+##############################################################################
+
+blastn.GRCh38 ()
 {
 
 # ----- Run blastn on filtered sequences from previous step ------
@@ -216,50 +236,39 @@ echo "Directory is set to" | tr '\n' ' ' && pwd
 mkdir -p annotation/blast/masurca
 mkdir -p annotation/blast/megahit
 
-echo "Start of BLASTN process"
+echo "[Start of BLASTN:GRCh38 Process]"
 
 # Masurca -----
 
 # Set directory to GRCh38 ------
 cd ../GRCh38/
 
+
 # BLAST against GRCh38.decoy.hla ------
 
-# In order for the following codes to run properly without warning, you need to create a hidden file named .ncbirc and
-# put it inside seqkit/masurca and seqkit/megahit
-# Contents of this file (remove the # from each line):
+echo "[Running BLAST+ on HG03563.masurca.filtered.fasta]"
 
-#; Start the section for BLAST configuration
-#[BLAST]
-#; Specifies the path where BLAST databases are installed
-#BLASTDB=/home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/GRCh38/
+start=`date +%s`  # record start time 
 
 # Run blast ------
+   blastn -db GRCh38_full_analysis_set_plus_decoy_hla.fa \
+   -query ../results/annotation/seqkit/masurca/HG03563.masurca.filtered.fasta \
+   -out ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.asn \
+   -outfmt 11 \
+   -max_target_seqs 5 \
+   -max_hsps 10 \
+   -evalue 1e-5 \
+   -perc_identity 90
 
-#  srun -A h3abionet -n 1 -N 1 --mem 20g -p lowmem --pty /bin/bash
+echo "[Convert BLAST Archive (.asn) to Tabular Format]"
 
-echo "RUNNING HG03563.masurca.filtered.fasta"
+#blast_formatter -archive ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.asn \
+#-outfmt "7 qseqid sseqid stitle pident length evalue qcovs bitscore sblastnames mismatch gapopen qstart qend qlen sstart send slen" \
+#> ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.tsv
 
-start=`date +%s`
-# Restrict number of hits -----
-    blastn -db GRCh38_full_analysis_set_plus_decoy_hla.fa \
-    -query ../results/annotation/seqkit/masurca/HG03563.masurca.filtered.fasta \
-    -out ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.asn \
-    -outfmt 11 \
-    -max_target_seqs 5 \
-    -max_hsps 10 \
-    -evalue 1e-5 \
-    -perc_identity 90
-
-echo "Convert blast archive .asn to tabular format"
-
-blast_formatter -archive ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.asn \
--outfmt "7 qseqid sseqid stitle pident length evalue qcovs bitscore sblastnames mismatch gapopen qstart qend qlen sstart send slen" \
-> ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.decoy.hla.tsv
-
-end=`date +%s`
-runtime=$((end-start))
-echo "$runtime"
+end=`date +%s` # record finish time 
+runtime=$( echo "scale=2;$((end-start)) / 60" | bc )
+echo "It took $runtime minutes to blast [HG03563.filtered.fasta] against [GRCh38.decoy.hla]"
 
 echo "Blasted HG03563.filtered.fasta against GRCh38.decoy.hla"
 
@@ -274,7 +283,7 @@ start=`date +%s`
 # Set directory to GRCh38.p0 ------
 cd ../GRCh38.p0/
 
-# Restrict number of hits -----
+# Run blast ------
     blastn -db GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
     -query ../results/annotation/seqkit/masurca/HG03563.masurca.filtered.fasta \
     -out ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.p0.no.decoy.hla.asn \
@@ -291,14 +300,104 @@ blast_formatter -archive ../results/annotation/blast/masurca/blast_HG03563.masur
 > ../results/annotation/blast/masurca/blast_HG03563.masurca.GRCh38.p0.no.decoy.hla.tsv
 
 end=`date +%s`
-runtime=$((end-start))
-echo "$runtime"
+runtime=$( echo "scale=2;$((end-start)) / 60" | bc )
+echo "It took $runtime minutes to blast [HG03563.filtered.fasta] against [GRCh38.p0.no.decoy.hla]"
 
 echo "Blasted HG03563.filtered.fasta against GRCh38.p0.no.decoy.hla"
-   
-echo "End of BLASTN process"
+
+echo "End of BLASTN GRCh38 process"
+
 }
 
+
+##############################################################################
+##																			                                    ##
+##	                 STEP 5: CREATE BLAST DATABASE (CHM13)                  ##
+##																		                                    	##	
+##############################################################################
+
+blastdb.CHM13()
+{
+
+# ----- Creat Blast database from CHM13 [GCA_009914755.3_CHM13_T2T_v1.1] + chrY of GRCh38.p13 [GCA_000001405.28_GRCh38.p13] -----
+
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/914/755/GCA_009914755.3_CHM13_T2T_v1.1/GCA_009914755.3_CHM13_T2T_v1.1_genomic.fna.gz
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.28_GRCh38.p13/GCA_000001405.28_GRCh38.p13_assembly_structure/Primary_Assembly/assembled_chromosomes/FASTA/chrY.fna.gz
+makeblastdb -in ../CHM13.v1.1_GRCh38.p13.chrY/CHM13.v1.1_GRCh38.p13.chrY.fna -parse_seqids -title "CHM13.v1.1_GRCh38.p13.chrY" -dbtype nucl
+
+# Building a new DB, current time: 09/15/2021 17:21:25
+# New DB name:   /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/CHM13.v1.1_GRCh38.p13.chrY/CHM13.v1.1_GRCh38.p13.chrY.fna
+# New DB title:  CHM13.v1.1_GRCh38.p13.chrY
+# Sequence type: Nucleotide
+# Deleted existing Nucleotide BLAST database named /home/groups/h3abionet/RefGraph/results/NeginV_Test_Summer2021/CHM13.v1.1_GRCh38.p13.chrY/CHM13.v1.1_GRCh38.p13.chrY.fna
+# Keep MBits: T
+# Maximum file size: 1000000000B
+# Adding sequences from FASTA; added 25 sequences in 25.1712 seconds.
+
+}
+
+##############################################################################
+##																			                                    ##
+##			        STEP 6: RUN BLAST ON FILTERED READS (CHM13)                 ##
+##																			                                    ##	
+##############################################################################
+
+blastn.CHM13 ()
+{
+
+# ----- Run blastn on filtered sequences from previous step ------
+
+# ----- Load necessary modules ------ 
+module load BLAST+/2.10.1-IGB-gcc-8.2.0
+
+# Set working directory -----
+cd ${OPT_d}
+echo "Directory is set to:"  |  tr '\n' ' ' && pwd 
+
+# Make a directory for blast ------
+mkdir -p annotation/blast/masurca
+mkdir -p annotation/blast/megahit
+
+echo "[Start of BLASTN:CHM13 Process]"
+
+# Masurca -----
+
+# Set directory to CHM13.v1.1_GRCh38.p13.chrY ------
+cd ../CHM13.v1.1_GRCh38.p13.chrY/
+
+
+# BLAST against GRCh38.decoy.hla ------
+
+echo "[Running BLAST+ on HG03563.masurca.filtered.fasta]"
+
+start=`date +%s`  # record start time 
+
+# Run blast ------
+    blastn -db CHM13.v1.1_GRCh38.p13.chrY.fna\
+    -query ../results/annotation/seqkit/masurca/HG03563.masurca.filtered.fasta \
+    -out ../results/annotation/blast/masurca/blast_HG03563.masurca.CHM13.v1.1_GRCh38.p13.chrY.fna.asn \
+    -outfmt 11 \
+    -max_target_seqs 5 \
+    -max_hsps 10 \
+    -evalue 1e-5 \
+    -perc_identity 90
+
+echo "Convert blast archive .asn to tabular format"
+
+blast_formatter -archive ../results/annotation/blast/masurca/blast_HG03563.masurca.CHM13.v1.1_GRCh38.p13.chrY.fna.asn \
+-outfmt "7 qseqid sseqid stitle pident length evalue qcovs bitscore sblastnames mismatch gapopen qstart qend qlen sstart send slen" \
+> ../results/annotation/blast/masurca/blast_HG03563.masurca.CHM13.v1.1_GRCh38.p13.chrY.fna.tsv
+
+end=`date +%s`
+runtime=$((end-start))
+runtime=$( echo "scale=2;$((end-start)) / 60" | bc )
+echo "It took $runtime minutes to blast [HG03563.filtered.fasta] against [CHM13.v1.1_GRCh38.p13.chrY]"
+
+echo "Blasted HG03563.filtered.fasta against CHM13.v1.1_GRCh38.p13.chrY.fna"
+
+echo "End of BLASTN CHM13 process"
+
+}
 
 ##############################################################################
 ##																			                                    ##
@@ -312,13 +411,16 @@ echo "End of BLASTN process"
 main()
 {
 	# Determine whether running full pipeline or single step
-	runtype="SINGLE STEP"
-
+	runtype="PARTIAL"
+  # runtype="FULL"
+  echo ""
 	echo "*** RUNNING ${runtype} ANNOTATION PIPELINE ***"
-    setup
-    #filter
-    #blastdbGRCh38
-   blastGRCh38
+  setup
+  #filter
+  # blastdb.GRCh38
+  # blastn.GRCh38
+  # blastdb.CHM13
+   blastn.CHM13
 }
 
 # Run main function
